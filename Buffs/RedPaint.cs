34308b940e6d9255a01765325bce052cdd7cc669
PaperLuigi <ethan3157@gmail.com>
2022-10-14 02:38:00 -0700

    Paint debuffs are now 'buffs' and are uncleansable.

--- Buffs/RedPaint.cs
+++ Buffs/RedPaint.cs
@@ -10,12 +10,17 @@ namespace StarsAbove.Buffs
             DisplayName.SetDefault("Red Paint");
             Description.SetDefault("You've been covered in red paint");
             Main.buffNoTimeDisplay[Type] = false;
-            Main.debuff[Type] = true; //Add this so the nurse doesn't remove the buff when healing
+            Main.debuff[Type] = false; //Add this so the nurse doesn't remove the buff when healing
         }
 
         public override void Update(Player player, ref int buffIndex)
         {
             player.GetModPlayer<StarsAbovePlayer>().RedPaint = true;
+        }
+        public override bool RightClick(int buffIndex)
+        {
+
+            return false;
         }
     }
 }
