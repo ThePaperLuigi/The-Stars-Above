9542eee51294c1118e9575cae298d666246f7f75
PaperLuigi <ethan3157@gmail.com>
2022-10-19 04:41:57 -0700

    Progress on Patreon weapon, 'El Capitan's Hardware'

--- Items/ElCapitansHardware.cs
+++ Items/ElCapitansHardware.cs
@@ -7,6 +7,7 @@ using StarsAbove.Projectiles;
 using static Terraria.ModLoader.ModContent;
 using StarsAbove.Items.Essences;
 using Terraria.Audio;
+using StarsAbove.Projectiles.ElCapitansHardware;
 
 namespace StarsAbove.Items
 {
@@ -16,11 +17,11 @@ namespace StarsAbove.Items
 			DisplayName.SetDefault("El Capitan's Hardware");
 			Tooltip.SetDefault("WORK IN PROGRESS WEAPON" +
                 "\nAttacks with this weapon pierce foes and bounce off walls up to 4 times" +
-				"\nStriking foes charges the [??? Gauge] (Critical strikes grant more charge)" +
-                "\nOnce the [??? Gauge] is above 50%, right-click to consume the [??? Gauge], swapping weapons and firing a [Gyro Disk]" +
-                "\nThe [Gyro Disk] targets and attacks up to 3 nearby foes, and can be moved by right-clicking" +
-                "\nAfter 10 seconds, the [Gyro Disk] disappears and the [??? Gauge] can gain energy again" +
-                "\nIf the [??? Gauge] is higher than 50% on activation, the [Gyro Disk] gains bonus damage proportional to the extra amount consumed" +
+				"\nStriking foes charges the [Renegade Gauge] (Critical strikes grant more charge)" +
+                "\nOnce the [Renegade Gauge] is above 50%, right-click to consume the [Renegade Gauge], swapping weapons and firing a [Gyro Disk] that lasts for 4 seconds" +
+                "\nThe [Gyro Disk] automatically targets and attacks nearby foes" +
+                "\nAfter 10 seconds, the [Gyro Disk] disappears and the [Renegade Gauge] can gain energy again" +
+                "\nIf the [Renegade Gauge] is higher than 50% on activation, the [Gyro Disk] gains bonus damage proportional to the extra amount consumed" +
 				"\n'GAME RIGHTS?!'" +
 				$"");
 
@@ -31,21 +32,24 @@ namespace StarsAbove.Items
 		public override void SetDefaults() {
 			Item.damage = 33;
 			Item.DamageType = DamageClass.Ranged;
-			Item.mana = 2;
 			Item.width = 40;
 			Item.height = 20;
-			Item.useTime = 5;
-			Item.useAnimation = 5;
+			Item.useTime = 12;
+			Item.useAnimation = 12;
 			Item.useStyle = 5;
 			Item.noMelee = true; //so the item's animation doesn't do damage
 			Item.knockBack = 4;
 			Item.rare = ItemRarityID.Lime;
 			Item.autoReuse = true;
-			Item.shoot = ProjectileType<HuckleberryRound>();
+			Item.shoot = ProjectileType<PierceGunShot>();
 			Item.shootSpeed = 16f;
 			Item.value = Item.buyPrice(gold: 1);           //The value of the weapon
+			Item.noUseGraphic = true;
 		}
-
+		public override bool AltFunctionUse(Player player)
+		{
+			return true;
+		}
 		public override void HoldItem(Player player)
 		{
 
@@ -54,22 +58,111 @@ namespace StarsAbove.Items
 		}
 		public override bool CanUseItem(Player player)
 		{
-			
-			return base.CanUseItem(player);
-		}
-
-		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
-		{
-			for (int d = 0; d < 27; d++)//Visual effects
+			var modPlayer = player.GetModPlayer<StarsAbovePlayer>();
+			if (player.altFunctionUse == 2)
 			{
-				Vector2 perturbedSpeed = new Vector2(velocity.X, velocity.Y).RotatedByRandom(MathHelper.ToRadians(55));
-				float scale = 3f - (Main.rand.NextFloat() * .3f);
-				perturbedSpeed = perturbedSpeed * scale;
-				int dustIndex = Dust.NewDust(position, 0, 0, 127, perturbedSpeed.X, perturbedSpeed.Y, 150, default(Color), 3f);
-				Main.dust[dustIndex].noGravity = true;
+				if (modPlayer.renegadeGauge >= 50)
+				{
+
+					
+					
+					return true;
+				}
+				else
+				{
+					return false;
+				}
 
 			}
-			return true;
+			else
+			{
+
+
+
+				return true;
+			}
+			return base.CanUseItem(player);
+		}
+		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
+		{
+			float launchSpeed = 120f;
+			float launchSpeed2 = 20f;
+			float launchSpeed3 = 5f;
+
+			Vector2 mousePosition = Main.MouseWorld;
+			Vector2 direction = Vector2.Normalize(mousePosition - player.Center);
+			Vector2 Gun = direction * launchSpeed2;
+			Vector2 Disk = direction * launchSpeed3;
+			Vector2 muzzleOffset = Vector2.Normalize(new Vector2(velocity.X, velocity.Y)) * 80f;
+			position = new Vector2(position.X, position.Y);
+			if (Collision.CanHit(position, 0, 0, position + muzzleOffset, 0, 0))
+			{
+				position += muzzleOffset;
+			}
+			if (player.altFunctionUse == 2)
+			{
+				Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem), position.X, position.Y, Gun.X, Gun.Y, ProjectileType<DroneGun>(), 0, knockback, player.whoAmI);
+				for (int d = 0; d < 27; d++)//Visual effects
+				{
+					Vector2 perturbedSpeed = new Vector2(velocity.X, velocity.Y).RotatedByRandom(MathHelper.ToRadians(25));
+					float scale = 2f - (Main.rand.NextFloat() * 1f);
+					perturbedSpeed = perturbedSpeed * scale;
+					int dustIndex = Dust.NewDust(position, 0, 0, DustID.FireworkFountain_Yellow, perturbedSpeed.X, perturbedSpeed.Y, 150, default(Color), 0.6f);
+					Main.dust[dustIndex].noGravity = true;
+
+				}
+				for (int d = 0; d < 27; d++)//Visual effects
+				{
+					Vector2 perturbedSpeed = new Vector2(velocity.X, velocity.Y).RotatedByRandom(MathHelper.ToRadians(125));
+					float scale = 1f - (Main.rand.NextFloat() * .6f);
+					perturbedSpeed = -(perturbedSpeed * scale);
+					int dustIndex = Dust.NewDust(position, 0, 0, DustID.GemTopaz, perturbedSpeed.X, perturbedSpeed.Y, 150, default(Color), 0.6f);
+					Main.dust[dustIndex].noGravity = true;
+
+				}
+			}
+			else
+			{
+
+
+				Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem), position.X, position.Y, Gun.X, Gun.Y, ProjectileType<PierceGun>(), 0, knockback, player.whoAmI);
+				for (int d = 0; d < 27; d++)//Visual effects
+				{
+					Vector2 perturbedSpeed = new Vector2(velocity.X, velocity.Y).RotatedByRandom(MathHelper.ToRadians(25));
+					float scale = 2f - (Main.rand.NextFloat() * 1f);
+					perturbedSpeed = perturbedSpeed * scale;
+					int dustIndex = Dust.NewDust(position, 0, 0, 127, perturbedSpeed.X, perturbedSpeed.Y, 150, default(Color), 1f);
+					Main.dust[dustIndex].noGravity = true;
+
+				}
+				for (int d = 0; d < 27; d++)//Visual effects
+				{
+					Vector2 perturbedSpeed = new Vector2(velocity.X, velocity.Y).RotatedByRandom(MathHelper.ToRadians(125));
+					float scale = 1f - (Main.rand.NextFloat() * .6f);
+					perturbedSpeed = -(perturbedSpeed * scale);
+					int dustIndex = Dust.NewDust(position, 0, 0, DustID.Smoke, perturbedSpeed.X, perturbedSpeed.Y, 150, default(Color), 1f);
+					Main.dust[dustIndex].noGravity = true;
+
+				}
+			}
+			
+			
+			SoundEngine.PlaySound(StarsAboveAudio.SFX_HuckleberryShoot, player.Center);
+			if (player.altFunctionUse == 2)
+			{
+				damage += (player.GetModPlayer<StarsAbovePlayer>().renegadeGauge - 50);
+				Projectile.NewProjectile(source, position.X, position.Y, Disk.X, Disk.Y, ProjectileType<GyroDisk>(), damage, knockback, player.whoAmI);
+				player.GetModPlayer<StarsAbovePlayer>().renegadeGauge = 0;
+			}
+			else
+			{
+
+
+				Projectile.NewProjectile(source, position.X, position.Y, velocity.X, velocity.Y, type, damage, knockback, player.whoAmI);
+				
+			}
+			
+			return false;
 		}
 
 		
