c679965d6084b96d3b664d750e27596d0badb30e
PaperLuigi <ethan3157@gmail.com>
2023-03-12 07:11:26 -0700

    Make wrap functions uniform

--- Dialogue/StarsAboveDialogueSystem.cs
+++ Dialogue/StarsAboveDialogueSystem.cs
@@ -8075,17 +8075,9 @@ namespace StarsAbove
 
             }//Warrior item
 
-            dialogue = Wrap(dialogue, 44);
+            dialogue = LangHelper.Wrap(dialogue, 44);
         }
 
-        private static string Wrap(string v, int size)
-        {
-            v = v.TrimStart();
-            if (v.Length <= size) return v;
-            var nextspace = v.LastIndexOf(' ', size);
-            if (-1 == nextspace) nextspace = Math.Min(v.Length, size);
-            return v.Substring(0, nextspace) + ((nextspace >= v.Length) ?
-            "" : "\n" + Wrap(v.Substring(nextspace), size));
-        }
+        
     }
 }
