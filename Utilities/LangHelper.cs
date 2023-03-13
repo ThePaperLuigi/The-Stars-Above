f2b8d3a7c865f77ecff82d0ed69d304a21d20660
MeireiHakuya <13926089879@163.com>
2023-03-13 17:16:57 +0800

    test

--- Utilities/LangHelper.cs
+++ Utilities/LangHelper.cs
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 ﻿using Terraria.Localization;
+=======
+﻿using System;
+using Terraria.Localization;
+>>>>>>> parent of beab749 (wrap method fix)
 using Terraria.ModLoader;
 
 namespace StarsAbove.Utilities;
@@ -14,4 +19,17 @@ internal static class LangHelper
     {
         return Language.GetTextValue($"Mods.{mod.Name}.{key}", args);
     }
+<<<<<<< HEAD
+=======
+
+    public static string Wrap(string v, int size)
+    {
+        v = v.TrimStart();
+        if (v.Length <= size) return v;
+        var nextspace = v.LastIndexOf(' ', size);
+        if (-1 == nextspace) nextspace = Math.Min(v.Length, size);
+        return v.Substring(0, nextspace) + ((nextspace >= v.Length) ?
+        "" : "\n" + Wrap(v.Substring(nextspace), size));
+    }
+>>>>>>> parent of beab749 (wrap method fix)
 }
