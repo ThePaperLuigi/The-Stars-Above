43ebc2ddad88704ec4bb63949c9028ac7ebf0f6b
MeireiHakuya <13926089879@163.com>
2023-03-13 17:18:17 +0800

    test

--- Utilities/LangHelper.cs
+++ Utilities/LangHelper.cs
@@ -22,6 +22,7 @@ internal static class LangHelper
 <<<<<<< HEAD
 =======
 
+<<<<<<< HEAD
     public static string Wrap(string v, int size)
     {
         v = v.TrimStart();
@@ -32,4 +33,54 @@ internal static class LangHelper
         "" : "\n" + Wrap(v.Substring(nextspace), size));
     }
 >>>>>>> parent of beab749 (wrap method fix)
+=======
+	private static string GetModTextValue(Mod mod, string key, params object[] args)
+	{
+		return Language.GetTextValue($"Mods.{mod.Name}.{key}", args);
+	}
+
+	//public static string Wrap(string v, int size)
+	//{
+	//    v = v.TrimStart();
+	//    if (v.Length <= size) return v;
+	//    var nextspace = v.LastIndexOf(' ', size);
+	//    if (-1 == nextspace) nextspace = Math.Min(v.Length, size);
+	//    return v.Substring(0, nextspace) + ((nextspace >= v.Length) ?
+	//    "" : "\n" + Wrap(v.Substring(nextspace), size));
+	//}
+
+	/// <summary>
+	/// Auto add newline according to <paramref name="limit" />
+	/// </summary>
+	/// <param name="text"> Input Text </param>
+	/// <param name="limit"> Character count limit </param>
+	/// <returns> </returns>
+	internal static string Wrap(ReadOnlySpan<char> text, int limit)
+	{
+		const int MaxNewLine = 8;
+
+		// Just try 2.2f and found it fits
+		limit = (GameCulture.CultureName)Language.ActiveCulture.LegacyId switch
+		{
+			GameCulture.CultureName.Chinese => (int)(limit / 2.2f),
+			_ => limit,
+		};
+		text = text.TrimStart();
+		int start = 0;
+		StringBuilder stringBuilder = new StringBuilder(text.Length + MaxNewLine);
+		while (limit + start <= text.Length)
+		{
+			// Maybe English dont lack of ' ' and Chinese dont contains ' '
+			int index = Math.Min(limit + start, text.Slice(start, limit).LastIndexOf(' ') & 0x7FFFFFFF);
+
+			// AppendLine dont support ReadOnlySpan???
+			stringBuilder.Append(text[start..index]).AppendLine();
+			start = index + 1;
+		}
+		stringBuilder.Append(text[start..]);
+
+		//Bad memory copy
+		return stringBuilder.ToString();
+	}
+>>>>>>> parent of 15de04d (small wrap fix)
 }
