ad197add27258337b9a7d1445ba736fb17f1a850
PaperLuigi <ethan3157@gmail.com>
2023-04-14 20:15:30 -0700

    Patreon weapon work

--- Items/SunsetOfTheSunGod.cs
+++ Items/SunsetOfTheSunGod.cs
@@ -9,6 +9,7 @@ using StarsAbove.Buffs;
 using StarsAbove.Items.Pets;
 using Terraria.Audio;
 using Terraria.GameContent.Creative;
+using StarsAbove.Projectiles.SunsetOfTheSunGod;
 
 namespace StarsAbove.Items
 {
@@ -22,8 +23,8 @@ namespace StarsAbove.Items
 				"\nRight click to unleash [Vasavi Shakti], launching yourself into the air and becoming Invincible (2 minute cooldown)" +
                 "\nAfter a short delay, channel an incredibly powerful beam of energy towards your cursor" +
                 "\n[Vasavi Shakti] will conclude with a colossal explosion, dealing 4x guaranteed critical damage and inflicting Daybroken" +
-				"\nThe current [c/F592BF:Aspected Damage Type] influences the weapon's attacks" +
-				"\n[c/FFAB4D:Melee]: Attacks rapidly stab three times; Charged attacks swing in a wide arc, burning foes for 8 seconds" +
+				"\nThe current [c/F592BF:Aspected Damage Type] influences the weapon's attacks (Weapon can not be used without [c/F592BF:Aspected Damage Type])" +
+				"\n[c/FFAB4D:Melee]: Rapidly stab in a close-ranged flurry; Charged attacks swing in a wide arc, burning foes for 8 seconds" +
 				"\n[c/EF4DFF:Magic]: Spears of lightning converge on your cursor, piercing up to 3 times" +
 				"\n[c/4DFF5B:Ranged]: Attacks launch the spear at high velocity, piercing infinitely" +
 				"\n[c/00CDFF:Summon]: Attacks swing the spear while extending a burning whip, causing summons to inflict Daybroken" +
@@ -36,23 +37,25 @@ namespace StarsAbove.Items
 
 		public override void SetDefaults()
 		{
-			Item.damage = 220;           //The damage of your weapon
-			Item.DamageType = DamageClass.Melee;          //Is your weapon a melee weapon?
-			Item.width = 46;            //Weapon's texture's width
-			Item.height = 216;           //Weapon's texture's height
-			Item.useTime = 35;          //The time span of using the weapon. Remember in terraria, 60 frames is a second.
-			Item.useAnimation = 35;         //The time span of the using animation of the weapon, suggest set it the same as useTime.
-			Item.useStyle = 1;          //The use style of weapon, 1 for swinging, 2 for drinking, 3 act like shortsword, 4 for use like life crystal, 5 for use staffs or guns
-			Item.knockBack = 12;         //The force of knockback of the weapon. Maximum is 20
+			Item.damage = 220;
+			Item.DamageType = DamageClass.Melee;
+			Item.useStyle = ItemUseStyleID.HiddenAnimation;
+			Item.width = 46;
+			Item.height = 216;
+			Item.useTime = 30;
+			Item.useAnimation = 30;
+			Item.knockBack = 4;         //The force of knockback of the weapon. Maximum is 20
 			Item.value = Item.buyPrice(gold: 1);           //The value of the weapon
 			Item.rare = ItemRarityID.Red;              //The rarity of the weapon, from -1 to 13
-			Item.UseSound = SoundID.Item15;      //The sound when the weapon is using
+			Item.UseSound = SoundID.Item1;      //The sound when the weapon is using
 			Item.autoReuse = true;          //Whether the weapon can use automatically by pressing mousebutton
 			Item.value = Item.buyPrice(gold: 1);           //The value of the weapon
-			Item.noUseGraphic = false;
-			Item.noMelee = false;
-			Item.shoot = ProjectileType<Projectiles.ArchitectLuminance.ArchitectShoot>();
-			Item.shootSpeed = 38;
+
+			Item.noUseGraphic = true;
+			Item.noMelee = true;
+
+			Item.shoot = ProjectileType<KarnaSpearAttack>();
+			Item.shootSpeed = 20;
 		}
 
 		public override bool AltFunctionUse(Player player)
@@ -62,28 +65,26 @@ namespace StarsAbove.Items
 
 		public override bool CanUseItem(Player player)
 		{
+			if (player.GetModPlayer<StarsAbovePlayer>().MeleeAspect != 2
+				&& player.GetModPlayer<StarsAbovePlayer>().MagicAspect != 2
+				&& player.GetModPlayer<StarsAbovePlayer>().RangedAspect != 2
+				&& player.GetModPlayer<StarsAbovePlayer>().SummonAspect != 2)
+			{
+
+				return false;
+			}
+			
 			if (player.altFunctionUse == 2)
 			{
-				if (!player.HasBuff(BuffType<ArtificeSirenBuff>()) && !player.HasBuff(BuffType<ArtificeSirenCooldown>()))
+				/*if (!player.HasBuff(BuffType<ArtificeSirenBuff>()) && !player.HasBuff(BuffType<ArtificeSirenCooldown>()))
 				{
-					player.AddBuff(BuffType<Buffs.ArtificeSirenBuff>(), 900);
-					//Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem),Main.MouseWorld, Vector2.Zero, mod.ProjectileType("ErinysFX"), 0, 0, player.whoAmI, 0, 1);
-					SoundEngine.PlaySound(StarsAboveAudio.SFX_summoning, player.Center);
-
-					Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem),Main.MouseWorld, Vector2.Zero, Mod.Find<ModProjectile>("Siren").Type, 0, 0, player.whoAmI);
-					Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem),Main.MouseWorld, Vector2.Zero, Mod.Find<ModProjectile>("SirenTurret1").Type, 0, 0, player.whoAmI);
-					Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem),Main.MouseWorld, Vector2.Zero, Mod.Find<ModProjectile>("SirenLaser1").Type, Item.damage, 0, player.whoAmI);
-					Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem),Main.MouseWorld, Vector2.Zero, Mod.Find<ModProjectile>("SirenTurret2").Type, 0, 0, player.whoAmI);
-					Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem),Main.MouseWorld, Vector2.Zero, Mod.Find<ModProjectile>("SirenLaser2").Type, Item.damage, 0, player.whoAmI);
-					Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem),Main.MouseWorld, Vector2.Zero, Mod.Find<ModProjectile>("SirenTurret3").Type, 0, 0, player.whoAmI);
-					Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem),Main.MouseWorld, Vector2.Zero, Mod.Find<ModProjectile>("SirenLaser3").Type, Item.damage, 0, player.whoAmI);
-
+					
 					return false;
 				}
 				else
 				{
 					return false;
-				}
+				}*/
 
 			}
 			
@@ -94,18 +95,29 @@ namespace StarsAbove.Items
 		{
 			
 		}
+		public int stabAmount;
+		public int stabTimer;
 		public override void HoldItem(Player player)
 		{
-			player.AddBuff(BuffType<Buffs.ArchitectLuminanceBuff>(), 2);
-			if (player.ownedProjectileCounts[ProjectileType<Projectiles.ArchitectLuminance.Armament>()] < 1)
+			float launchSpeed = 8f;
+			Vector2 mousePosition = Main.MouseWorld;
+			Vector2 direction = Vector2.Normalize(mousePosition - player.Center);
+			Vector2 arrowVelocity = direction * launchSpeed;
+			Vector2 perturbedSpeed = new Vector2(arrowVelocity.X, arrowVelocity.Y).RotatedByRandom(MathHelper.ToRadians(25));
+			if (stabAmount > 0)
 			{
-				Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem),player.position.X, player.position.Y, 0, 0, ProjectileType<Projectiles.ArchitectLuminance.Armament>(), 0, 4, player.whoAmI, 0f);
+				stabTimer--;
+				if (stabTimer <= 0)
+				{
+					stabAmount--;
 
-
+					Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem), player.position.X, player.position.Y, perturbedSpeed.X, perturbedSpeed.Y, ProjectileType<KarnaSpearAttack>(), player.GetWeaponDamage(Item), 0, player.whoAmI, 0f);
+					stabTimer = 10;
+				}
 			}
+			/*
 			if (player.GetModPlayer<StarsAbovePlayer>().MeleeAspect == 2)
 			{
-				Item.useStyle = 1;
 				
 				Item.useTime = 20;          //The time span of using the weapon. Remember in terraria, 60 frames is a second.
 				Item.useAnimation = 20;
@@ -135,8 +147,8 @@ namespace StarsAbove.Items
 				Item.useAnimation = 35;
 				Item.UseSound = SoundID.Item15;
 			}
-
-				base.HoldItem(player);
+			*/
+			base.HoldItem(player);
 		}
 		public override void OnHitNPC(Player player, NPC target, int damage, float knockback, bool crit)
 		{
@@ -148,29 +160,26 @@ namespace StarsAbove.Items
 
 		public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
 		{
+			if (player.GetModPlayer<StarsAbovePlayer>().MeleeAspect == 2)
+			{
+				stabAmount = 3;
+				stabTimer = 0;
+				
+			}
+			if (player.GetModPlayer<StarsAbovePlayer>().SummonAspect == 2)
+			{
+
+
+			}
 			if (player.GetModPlayer<StarsAbovePlayer>().RangedAspect == 2)
             {
 
-				Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem),position.X, position.Y, velocity.X, velocity.Y,ProjectileID.MoonlordBullet, damage/2, knockback, player.whoAmI);
 				
-				Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem),player.MountedCenter.X, player.MountedCenter.Y, velocity.X/2, velocity.Y/2, ProjectileType<Projectiles.ArchitectLuminance.ArchitectShoot>(), 0, 3, player.whoAmI, 0f);
-
-
 			}
 			if (player.GetModPlayer<StarsAbovePlayer>().MagicAspect == 2)
 			{
 
-				float numberProjectiles = 6;
-				float rotation = MathHelper.ToRadians(15);
-				position += Vector2.Normalize(new Vector2(velocity.X, velocity.Y)) * 15f;
-				for (int i = 0; i < numberProjectiles; i++)
-				{
-					Vector2 perturbedSpeed = new Vector2(velocity.X, velocity.Y).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * .2f; // Watch out for dividing by 0 if there is only 1 projectile.
-					Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem),position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, ProjectileID.LunarFlare, damage/3 , knockback, player.whoAmI);
-				}
-				Projectile.NewProjectile(player.GetSource_ItemUse(player.HeldItem),player.MountedCenter.X, player.MountedCenter.Y, velocity.X/2, velocity.Y/2, ProjectileType<Projectiles.ArchitectLuminance.ArchitectShoot>(), 0, 3, player.whoAmI, 0f);
-
-
+				
 			}
 			return false;
 		}
---
+++ Projectiles/SunsetOfTheSunGod/KarnaSpearAttack.cs
@@ -0,0 +1,143 @@
+﻿
+using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.Audio;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace StarsAbove.Projectiles.SunsetOfTheSunGod
+{
+    public class KarnaSpearAttack : ModProjectile
+	{
+		public override void SetStaticDefaults() {
+			DisplayName.SetDefault("Sunset of the Sun God");
+
+		}
+
+		public override void SetDefaults() {
+			Projectile.width = 220;
+			Projectile.height = 220;
+			Projectile.aiStyle = -1;
+			Projectile.penetrate = -1;
+			Projectile.scale = 1f;
+			Projectile.timeLeft = 15;
+			Projectile.alpha = 0;
+			Projectile.DamageType = DamageClass.Magic;
+			Projectile.hide = false;
+			Projectile.ownerHitCheck = true;
+			Projectile.tileCollide = false;
+			Projectile.friendly = true;
+			Projectile.usesLocalNPCImmunity = true;
+			Projectile.localNPCHitCooldown = -1;
+		}
+
+		public bool firstSpawn = true;
+		
+		public float movementFactor 
+		{
+			get => Projectile.ai[0];
+			set => Projectile.ai[0] = value;
+		}
+
+		
+		public override void AI() {
+			Player projOwner = Main.player[Projectile.owner];
+			projOwner.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, (projOwner.Center - Projectile.Center).ToRotation() + MathHelper.PiOver2);
+
+			DelegateMethods.v3_1 = new Vector3(0.6f, 1f, 1f) * 0.2f;
+			Utils.PlotTileLine(Projectile.Center, Projectile.Center + Projectile.velocity * 10f, 8f, DelegateMethods.CastLightOpen);
+			
+			
+			// Here we set some of the projectile's owner properties, such as held item and itemtime, along with projectile direction and position based on the player
+			Vector2 ownerMountedCenter = projOwner.RotatedRelativePoint(projOwner.MountedCenter, true);
+			Projectile.direction = projOwner.direction;
+			projOwner.heldProj = Projectile.whoAmI;
+			projOwner.itemTime = projOwner.itemAnimation;
+			Projectile.position.X = projOwner.Center.X - (float)(Projectile.width / 2);
+			Projectile.position.Y = projOwner.Center.Y - (float)(Projectile.height / 2);
+			// As long as the player isn't frozen, the spear can move
+			if (!projOwner.frozen) {
+				if (movementFactor == 0f) // When initially thrown out, the ai0 will be 0f
+				{
+					movementFactor = 1f; // Make sure the spear moves forward when initially thrown out
+					Projectile.netUpdate = true; // Make sure to netUpdate this spear
+				}
+				if (Projectile.timeLeft < 10) // Somewhere along the item animation, make sure the spear moves back
+				{
+					
+					movementFactor += 0.4f;
+					Projectile.alpha += 20;
+				}
+				else // Otherwise, increase the movement factor
+				{
+					movementFactor += 3.1f;
+				}
+			}
+			// Change the spear position based off of the velocity and the movementFactor
+			Projectile.position += Projectile.velocity * movementFactor;
+			// When we reach the end of the animation, we can kill the spear projectile
+			if (Projectile.alpha > 250) {
+				float dustAmount = 16f;
+
+				for (int i = 0; (float)i < dustAmount; i++)
+				{
+					Vector2 spinningpoint5 = Vector2.UnitX * 0f;
+					spinningpoint5 += -Vector2.UnitY.RotatedBy((float)i * ((float)Math.PI * 2f / dustAmount)) * new Vector2(1f, 4f);
+					spinningpoint5 = spinningpoint5.RotatedBy(Projectile.velocity.ToRotation());
+					int dust = Dust.NewDust(Projectile.Center, 0, 0, DustID.GemTopaz);
+					Main.dust[dust].scale = 1f;
+					Main.dust[dust].noGravity = true;
+					Main.dust[dust].position = Projectile.Center + spinningpoint5;
+					Main.dust[dust].velocity = Projectile.velocity * 0f + spinningpoint5.SafeNormalize(Vector2.UnitY) * 1f;
+				}
+				Projectile.Kill();
+			}
+			// Apply proper rotation, with an offset of 135 degrees due to the sprite's rotation, notice the usage of MathHelper, use this class!
+			// MathHelper.ToRadians(xx degrees here)
+			Projectile.rotation = Projectile.velocity.ToRotation();//+ MathHelper.ToRadians(135f)
+																   // Offset by 90 degrees here
+			if (Projectile.spriteDirection == -1)
+			{
+				Projectile.rotation -= MathHelper.ToRadians(180f);
+			}
+			Projectile.spriteDirection = Projectile.direction;
+
+			if (firstSpawn)
+			{
+				firstSpawn = false;
+				SoundEngine.PlaySound(SoundID.Item1, Projectile.Center);
+
+				float dustAmount = 16f;
+				for (int i = 0; (float)i < dustAmount; i++)
+				{
+					Vector2 spinningpoint5 = Vector2.UnitX * 0f;
+					spinningpoint5 += -Vector2.UnitY.RotatedBy((float)i * ((float)Math.PI * 2f / dustAmount)) * new Vector2(1f, 4f);
+					spinningpoint5 = spinningpoint5.RotatedBy(Projectile.velocity.ToRotation());
+					int dust = Dust.NewDust(Projectile.Center, 0, 0, DustID.GemTopaz);
+					Main.dust[dust].scale = 1f;
+					Main.dust[dust].noGravity = true;
+					Main.dust[dust].position = Projectile.Center + spinningpoint5;
+					Main.dust[dust].velocity = Projectile.velocity * 0f + spinningpoint5.SafeNormalize(Vector2.UnitY) * 3f;
+				}
+
+				
+			}
+
+			
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			for (int d = 0; d < 8; d++)
+			{
+				Dust.NewDust(target.Center, 0, 0, DustID.LifeDrain, Main.rand.NextFloat(-5, 5), Main.rand.NextFloat(-5, 5), 150, default(Color), 0.4f);
+				Dust.NewDust(target.Center, 0, 0, DustID.FireworkFountain_Red, Main.rand.NextFloat(-8, 8), Main.rand.NextFloat(-8, 8), 150, default(Color), 0.6f);
+			}
+
+			base.OnHitNPC(target, damage, knockback, crit);
+		}
+	}
+
+	
+}
