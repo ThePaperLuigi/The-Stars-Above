724b78ad503205d8fd7d572edf8fea67935b3b3b
PaperLuigi <ethan3157@gmail.com>
2023-06-08 02:20:34 -0700

    New accessories

--- Items/Accessories/SpellcasterSigil.cs
+++ Items/Accessories/SpellcasterSigil.cs
@@ -1,19 +1,15 @@
 ﻿using StarsAbove.Systems;
+using StarsAbove.Systems;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
 namespace StarsAbove.Items.Accessories
 {
-    public class GaleflameFeather : ModItem
+    public class SpellcasterSigil : ModItem
 	{
 		public override void SetStaticDefaults() {
-			// DisplayName.SetDefault("Galeflame Feather");
-
-			/* Tooltip.SetDefault("" +
-				"\nGain 5% increased damage in the air" +
-				"\nImmunity frames in the air grant an additional 18% increased damage and Swiftness"+
-				"\n'Some may say it's overused'"); */
+			
 			Terraria.GameContent.Creative.CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
 			//The (English) text shown below your weapon's name
 		}
@@ -27,18 +23,7 @@ namespace StarsAbove.Items.Accessories
 		}
 
 		public override void UpdateAccessory(Player player, bool hideVisual) {
-			player.GetModPlayer<WeaponPlayer>().GaleflameFeather = true;
-			if(player.velocity.Y != 0)
-            {
-				player.GetDamage(DamageClass.Generic) += 0.05f;
-				if(player.immune)
-                {
-					player.GetDamage(DamageClass.Generic) += 0.18f;
-					player.AddBuff(BuffID.Swiftness, 2);
-                }
-            }
-			//player.respawnTimer += 600;
-
+			
 		}
 
 		
