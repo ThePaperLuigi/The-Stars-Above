44dd917915baab26855a25ed2f516bb64f77949e
PaperLuigi <ethan3157@gmail.com>
2023-06-27 05:06:20 -0700

    All Stellar Spoils can be shimmered into a Stellar Remnant

--- Systems/StarsAboveGlobalItem.cs
+++ Systems/StarsAboveGlobalItem.cs
@@ -532,7 +532,58 @@ namespace StarsAbove
 					}
 				}
 			}
-			
+			if (item.OriginalRarity == ModContent.GetInstance<StellarSpoilsRarity>().Type)
+			{
+				if (!line.OneDropLogo)
+				{
+					// You are not allowed to change these, modders should use ModifyTooltips to modify them
+					//line.text = "you shall not pass...";
+					//line.oneDropLogo = false;
+					//line.color = Color.AliceBlue;
+					//line.overrideColor = Color.AliceBlue;
+					//line.isModifier = false;
+					//line.isModifierBad = false;
+					//line.index = 1;
+
+					// Let's draw the item name centered so it's in the middle, and let's add a form of separator
+					string sepText = "✦"; // This is our separator, which will go between the item name and the rest
+					float sepHeight = line.Font.MeasureString(sepText).Y; // Height of our separator
+
+					// If our line text equals our item name, this is our tooltip line for the item name
+					// if (line.text == item.HoverName)
+					// What is more accurate to check is the layer name and mod
+					if (line.Name == "ItemName" && line.Mod == "Terraria")
+					// We check for Terraria so we modify the vanilla tooltip and not a modded one
+					// This could be important, in case some mod does a lot of custom work and removes the standard tooltip
+					// For tooltip layers, check the documentation for TooltipLine
+					{
+						// Our offset is half the width of our box, minus the padding of one side
+						//float boxOffset = boxSize.X / 2 - paddingForBox;
+						// The X coordinate where we draw is where the line would draw, plus the box offset,
+						// which would place the START of the string at the center, so we subtract half of the line width to center it completely
+						float drawX = line.X + (int)line.Font.MeasureString(line.Text).X / 2 - line.Font.MeasureString(sepText).X / 2;
+						float drawY = line.Y + sepHeight / 2;
+
+						// Note how our line object has many properties we can use for drawing
+						// Here we draw the separator, note that it'd make more sense to use PostDraw for this, but either will work
+						ChatManager.DrawColorCodedStringWithShadow(Main.spriteBatch, line.Font, sepText,
+							new Vector2(drawX, drawY), line.Color, line.Rotation, line.Origin, line.BaseScale, line.MaxWidth, line.Spread);
+
+						// Here we do the same thing as we did for drawX, which will center our ItemName tooltip
+						//line.X += (int)boxOffset - (int)line.Font.MeasureString(line.Text).X / 2;
+						// yOffset affects the offset that is added every next line, so this will cause the line to come after the separator to be drawn slightly lower
+						yOffset = (int)sepHeight / 4;
+					}
+					else
+					{
+						// Reset the offset for other lines
+						//float boxOffset = boxSize.X / 2 - paddingForBox;
+						//line.X = 0;
+						yOffset = 0;
+						//line.X += (int)boxOffset - (int)line.Font.MeasureString(line.Text).X / 2;
+					}
+				}
+			}
 			return true;
         }
         public override bool PreDrawInInventory(Item item, SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
@@ -640,7 +691,19 @@ namespace StarsAbove
 
 			return true;
 		}
-
+		public override void AddRecipes()
+        {
+			for(int i = 0; i < Main.maxItems; i++)
+            {
+				Item item = Main.item[i];
+				if(item.OriginalRarity == ModContent.GetInstance<StellarSpoilsRarity>().Type)
+                {
+					Recipe recipe = Recipe.Create(item.type)
+					.AddCustomShimmerResult(ModContent.ItemType<StellarRemnant>())
+					.Register();
+				}
+            }
+        }
         public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
 		{
 			if (AstralWeapons.Contains(item.type))
@@ -656,6 +719,12 @@ namespace StarsAbove
 			if (SpatialWeapons.Contains(item.type))
 			{
 				TooltipLine tooltip = new TooltipLine(Mod, "StarsAbove: SpatialIdentifier", $"[i:{ItemType<Spatial>()}]") { OverrideColor = Color.White };
+				tooltips.Add(tooltip);
+				//
+			}
+			if (item.OriginalRarity == ModContent.GetInstance<StellarSpoilsRarity>().Type)
+			{
+				TooltipLine tooltip = new TooltipLine(Mod, "StarsAbove: StellarSpoils", LangHelper.GetTextValue("Common.CanBeShimmeredSpoils")) { OverrideColor = Color.White };
 				tooltips.Add(tooltip);
 				//
 			}
