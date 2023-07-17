a60d64b3107143d18187e7be848c52cdcd80b55a
PaperLuigi <ethan3157@gmail.com>
2023-07-16 23:50:20 -0700

    Gave Stellar Remnant recipe to Music boxes and changed rarity to Stellar Spoils

--- Items/Placeable/MageOfVioletMusicBox.cs
+++ Items/Placeable/MageOfVioletMusicBox.cs
@@ -2,6 +2,7 @@ using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 using Terraria.ID;
 using StarsAbove.Items.Prisms;
+using StarsAbove.Systems;
 
 namespace StarsAbove.Items.Placeable
 {
@@ -31,15 +32,16 @@ namespace StarsAbove.Items.Placeable
 			Item.createTile = Mod.Find<ModTile>("MageOfVioletMusicBox").Type;
 			Item.width = 24;
 			Item.height = 24;
-			Item.rare = 10;
+			Item.rare = ModContent.GetInstance<StellarSpoilsRarity>().Type; // Custom Rarity
 			Item.value = 1;
 			Item.accessory = true;
 		}
 		public override void AddRecipes()
 		{
 			CreateRecipe(1)
-				.AddIngredient(ItemType<PrismaticCore>(), 8)
-				.AddTile(TileID.WorkBenches)
+				.AddIngredient(ModContent.ItemType<Materials.StellarRemnant>(), 20)
+				.AddCustomShimmerResult(ModContent.ItemType<Materials.StellarRemnant>(), 3)
+				.AddTile(Terraria.ID.TileID.Anvils)
 				.Register();
 		}
 	}
