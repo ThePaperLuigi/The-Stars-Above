958c12ac5bca03a043b67d51eeddf5170628f2e2
PaperLuigi <ethan3157@gmail.com>
2023-07-16 23:46:23 -0700

    Added new Starpop Vanity set

---
+++ Items/Vanity/Starpop/StarpopLegs.cs
@@ -0,0 +1,42 @@
+using Terraria;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+using Terraria.ID;
+using StarsAbove.Items.Materials;
+using StarsAbove.Items.Prisms;
+using Terraria.GameContent.Creative;
+using StarsAbove.Systems;
+
+namespace StarsAbove.Items.Vanity.Starpop
+
+{
+    [AutoloadEquip(EquipType.Legs)]
+	public class StarpopLegs : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			// DisplayName.SetDefault("Stargazing Hero's Greaves");
+			// Tooltip.SetDefault("Spatial garb of ages past");
+
+			CreativeItemSacrificesCatalog.Instance.SacrificeCountNeededByItemId[Type] = 1;
+
+		}
+
+		public override void SetDefaults()
+		{
+			Item.width = 28;
+			Item.height = 24;
+			Item.value = 1;
+			Item.rare = ModContent.GetInstance<StellarSpoilsRarity>().Type; // Custom Rarity
+			Item.vanity = true;
+		}
+		public override void AddRecipes()
+		{
+			CreateRecipe(1)
+				.AddIngredient(ModContent.ItemType<Materials.StellarRemnant>(), 20)
+				.AddCustomShimmerResult(ModContent.ItemType<Materials.StellarRemnant>(), 3)
+				.AddTile(Terraria.ID.TileID.Anvils)
+				.Register();
+		}
+	}
+}
