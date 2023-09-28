56addd6d8e2b324e4988592f83143739642dc100
PaperLuigi <ethan3157@gmail.com>
2023-09-28 04:36:45 -0700

    Begun work on the Weapon Memory system

---
+++ Systems/Items/ItemMemorySystem.cs
@@ -0,0 +1,51 @@
+﻿
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using StarsAbove.Items;
+using StarsAbove.Items.Weapons;
+using StarsAbove.Items.Weapons.Summon;
+using StarsAbove.Items.Weapons.Ranged;
+using StarsAbove.Items.Weapons.Other;
+using StarsAbove.Items.Weapons.Celestial;
+using StarsAbove.Items.Weapons.Melee;
+using StarsAbove.Items.Weapons.Magic;
+using StarsAbove.Items.Essences;
+using StarsAbove.Prefixes;
+using SubworldLibrary;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using StarsAbove.Items.Prisms;
+using static Terraria.ModLoader.ModContent;
+using Terraria.GameContent;
+using StarsAbove.Items.Armor.StarfarerArmor;
+using StarsAbove.Items.Materials;
+using StarsAbove.Utilities;
+using Terraria.UI.Chat;
+using StarsAbove.Buffs;
+using StarsAbove.Buffs.StellarNovas;
+using StarsAbove.Items.Loot;
+using StarsAbove.Systems;
+using Terraria.DataStructures;
+
+namespace StarsAbove.Systems.Items
+{
+
+    public class ItemMemorySystem : GlobalItem
+    {
+        public override bool InstancePerEntity => true;
+        public override GlobalItem Clone(Item item, Item itemClone)
+        {
+            return base.Clone(item, itemClone);
+        }
+        //Memories roll with random starting stats that are distributed to the item they're applied to.
+        public override void OnCreated(Item item, ItemCreationContext context)
+        {
+            base.OnCreated(item, context);
+        }
+
+
+    }
+}
