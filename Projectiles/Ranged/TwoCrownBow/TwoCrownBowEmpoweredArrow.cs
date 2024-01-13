f25dcff8d4607a517942949afe7bc0938c9fdbae
PaperLuigi <ethan3157@gmail.com>
2024-01-12 16:51:16 -0800

    Two-Crown Bow cleanup

--- Projectiles/Ranged/TwoCrownBow/TwoCrownBowEmpoweredArrow.cs
+++ Projectiles/Ranged/TwoCrownBow/TwoCrownBowEmpoweredArrow.cs
@@ -28,7 +28,7 @@ namespace StarsAbove.Projectiles.Ranged.TwoCrownBow
             Projectile.aiStyle = 1;             //The ai style of the projectile, please reference the source code of Terraria
             Projectile.friendly = true;         //Can the projectile deal damage to enemies?
             Projectile.hostile = false;         //Can the projectile deal damage to the player?
-            Projectile.penetrate = 1;           //How many monsters the projectile can penetrate. (OnTileCollide below also decrements penetrate for bounces as well)
+            Projectile.penetrate = 99;           //How many monsters the projectile can penetrate. (OnTileCollide below also decrements penetrate for bounces as well)
             Projectile.timeLeft = 60;          //The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
             Projectile.alpha = 125;             //The transparency of the projectile, 255 for completely transparent. (aiStyle 1 quickly fades the projectile in) Make sure to delete this if you aren't using an aiStyle that fades in. You'll wonder why your projectile is invisible.
             Projectile.light = 0.5f;            //How much light emit around the projectile
