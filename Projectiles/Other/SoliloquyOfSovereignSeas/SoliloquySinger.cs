52d34cef322c424eefcad32ec6cf6671cea9728f
PaperLuigi <ethan3157@gmail.com>
2024-02-11 16:17:30 -0800

    Changed weapon healing and fixed missing minion slot

--- Projectiles/Other/SoliloquyOfSovereignSeas/SoliloquySinger.cs
+++ Projectiles/Other/SoliloquyOfSovereignSeas/SoliloquySinger.cs
@@ -26,6 +26,7 @@ namespace StarsAbove.Projectiles.Other.SoliloquyOfSovereignSeas
 			Projectile.CloneDefaults(ProjectileID.DD2PetGhost);
 			AIType = ProjectileID.DD2PetGhost;
 			Projectile.light = 1f;
+            Projectile.minionSlots = 1f;
 		}
 
 		public override bool PreAI()
@@ -60,7 +61,16 @@ namespace StarsAbove.Projectiles.Other.SoliloquyOfSovereignSeas
                     Player p = Main.player[i];
                     if (p.active && !p.dead && p.Distance(Projectile.Center) < 490f)
                     {
-                        p.Heal((int)(Projectile.damage * 0.05f));
+                        if(p.statLife < 200)
+                        {
+                            p.Heal((int)(Projectile.damage * 0.1f));
+
+                        }
+                        else
+                        {
+                            p.Heal((int)(Projectile.damage * 0.05f));
+
+                        }
                     }
 
                 }
