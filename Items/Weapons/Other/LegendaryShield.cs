c9196ad2852ecf314f073e6196ab4ff2bb7c9b88
PaperLuigi <ethan3157@gmail.com>
2024-03-15 03:29:15 -0700

    Update LegendaryShield.cs

--- Items/Weapons/Other/LegendaryShield.cs
+++ Items/Weapons/Other/LegendaryShield.cs
@@ -2,7 +2,9 @@ using Microsoft.Xna.Framework;
 using StarsAbove.Buffs.Farewells;
 using StarsAbove.Items.Essences;
 using StarsAbove.Items.Materials;
+using StarsAbove.Projectiles.Other.LegendaryShield;
 using StarsAbove.Projectiles.Summon.KeyOfTheKingsLaw;
+using StarsAbove.Projectiles.Summon.Wavedancer;
 using StarsAbove.Systems;
 using StarsAbove.Systems;
 using Terraria;
@@ -24,19 +26,19 @@ namespace StarsAbove.Items.Weapons.Other
 
 		public override void SetDefaults()
 		{
-			Item.damage = 0;           //Not a weapon.
+			Item.damage = 50;
 			//Item.mana = 80;
 			//Item.DamageType = DamageClass.Summon;          //Is your weapon a melee weapon?
 			Item.width = 40;            //Weapon's texture's width
 			Item.height = 40;           //Weapon's texture's height
-			Item.useTime = 60;          //The time span of using the weapon. Remember in terraria, 60 frames is a second.
-			Item.useAnimation = 60;         //The time span of the using animation of the weapon, suggest set it the same as useTime.
+			Item.useTime = 20;          //The time span of using the weapon. Remember in terraria, 60 frames is a second.
+			Item.useAnimation = 20;         //The time span of the using animation of the weapon, suggest set it the same as useTime.
 			Item.useStyle = ItemUseStyleID.HoldUp;          //The use style of weapon, 1 for swinging, 2 for drinking, 3 act like shortsword, 4 for use like life crystal, 5 for use staffs or guns
 			Item.knockBack = 6;         //The force of knockback of the weapon. Maximum is 20
 			Item.rare = ItemRarityID.Green;              //The rarity of the weapon, from -1 to 13
 			//Item.UseSound = StarsAboveAudio.SFX_AgnianTune;      //The sound when the weapon is using
 			Item.autoReuse = true;          //Whether the weapon can use automatically by pressing mousebutton
-			Item.shoot = ProjectileType<KeyOfTheKingsLawProjectile>();
+			Item.shoot = ProjectileType<LegendaryShieldBash>();
 			Item.shootSpeed = 15f;
 			Item.value = Item.buyPrice(gold: 1);           //The value of the weapon
 		}
@@ -45,7 +47,7 @@ namespace StarsAbove.Items.Weapons.Other
 		int randomBuff;
         public override void HoldItem(Player player)
         {
-
+			player.GetModPlayer<WeaponPlayer>().LegendaryShieldHeld = true;
             base.HoldItem(player);
         }
         public override bool CanUseItem(Player player)
@@ -75,8 +77,10 @@ namespace StarsAbove.Items.Weapons.Other
         }
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
-		{
-			return false;
+        {
+            Projectile.NewProjectile(player.GetSource_FromThis(), player.Center, Vector2.Zero, ProjectileType<LegendaryShieldBash>(), damage, 0, player.whoAmI);
+
+            return false;
 		}
 		public override void AddRecipes()
 		{
