77a0b734c1937da4f32b33f93f2f45a81976997b
PaperLuigi <ethan3157@gmail.com>
2024-06-21 19:13:59 -0700

    Testing new prism types

---
+++ Items/Prisms/PyrogenicPrism.cs
@@ -0,0 +1,22 @@
+using Microsoft.Xna.Framework;
+using StarsAbove.Systems.Items;
+using StarsAbove.Utilities;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace StarsAbove.Items.Prisms
+{
+    public class PyrogenicPrism : StellarPrism
+    {
+        public override int SetBonus { get; set; } = (int)ItemPrismSystem.MajorSetBonuses.CrescentMeteor;
+        public override string SetBonusSimpleName { get; set; } = "CrescentMeteor";
+        public override string SetBonusName { get; set; } = LangHelper.GetTextValue("StellarNova.StellarPrisms.CrescentMeteor.Name");
+        public override string SetBonusDescription1 { get; set; } = LangHelper.GetTextValue("StellarNova.StellarPrisms.CrescentMeteor.SetBonus1");
+        public override string SetBonusDescription2 { get; set; } = LangHelper.GetTextValue("StellarNova.StellarPrisms.CrescentMeteor.SetBonus2");
+        public override string SetBonusDescription3 { get; set; } = LangHelper.GetTextValue("StellarNova.StellarPrisms.CrescentMeteor.SetBonus3");
+
+        public override string FlavorTooltip { get; set; } = LangHelper.GetTextValue("StellarNova.StellarPrisms.CrescentMeteor.FlavorTooltip");
+
+    }
+}
